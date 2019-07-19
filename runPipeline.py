f7da87904e492ac4c492c9dfeb659186fa93b7a4
Jordan Force <jordan.force@uconn.edu>
2019-07-19 12:01:01 -0400

    done

--- runPipeline.py
+++ runPipeline.py
@@ -8,7 +8,6 @@ import uuid
 parser = argparse.ArgumentParser()
 
 parser.add_argument('--base_project', action='append')
-parser.add_argument('--base_proteome', action='append')
 parser.add_argument('--allele', action='append')
 parser.add_argument('--additional_proteome', action='append')
 parser.add_argument('--pep_len', type=str)
@@ -19,15 +18,15 @@ parser.add_argument('--mgf', type=str)
 parser.add_argument('--output', type=str)
 
 
+
 args = parser.parse_args()
 tools_location = '/galaxy-prod/galaxy/tools-dependencies/bin/MSEpitope/tidePipeline'
 allele_list = []
 for x in args.allele:
     allele_list.append('--allele')
     allele_list.append(x)
-project_directory = os.path.join(os.getcwd(), 'project')
+project_directory = os.path.join(os.getcwd(), 'project', 'project')
 assert(len(set(args.base_project)) == 1)
-assert(len(set(args.proteomes)) == 1)
 base_project = args.base_project[0]
 p = subprocess.Popen(['python3', 'CopyProject.py', base_project, project_directory] + allele_list, cwd=tools_location, stderr=sys.stdout.fileno())
 
@@ -36,10 +35,11 @@ assert(p.wait() == 0)
 for allele in args.allele:
     for pep_len in args.pep_len.split(','):
         netmhc_name = 'Human' + pep_len + 'Mers_' + allele
-        p = subprocess.Popen(['python3', 'FilterNetMHC.py', project_directory, netmhc_name, args.rank, netmhc_name + '_filtered'], cwd=tools_location, stderr=sys.stdout.fileno())
+        p = subprocess.Popen(['python3', 'FilterNetMHC.py', project_directory, netmhc_name, args.rank_filter, netmhc_name + '_filtered'], cwd=tools_location, stderr=sys.stdout.fileno())
         assert(p.wait() == 0)
         filtered_netmhc_names.append('--FilteredNetMHC')
         filtered_netmhc_names.append(netmhc_name + '_filtered')
+
 
 print('current working: ' + os.getcwd())
 print('listdir')
@@ -58,11 +58,11 @@ for x in args.allele:
     assert(p.wait() == 0)
 """
 
-mgf_link = os.path.join(project_directory, 'thing.mgf')
-os.link(args.mgf, mgf_link)
+#mgf_link = os.path.join(project_directory, 'thing.mgf')
+#os.link(args.mgf, mgf_link)
 
 
-p = subprocess.Popen(['python3', 'AddMGF.py', project_directory, mgf_link, 'mgf', '8', args.frag_method, args.instrument], cwd=tools_location, stderr=sys.stdout.fileno())
+p = subprocess.Popen(['python3', 'AddMGF.py', project_directory, args.mgf, 'mgf', '8', args.frag_method, args.instrument], cwd=tools_location, stderr=sys.stdout.fileno())
 assert(p.wait() == 0)
 
 
@@ -82,16 +82,20 @@ if args.additional_proteome:
 
     for x in args.allele:
         for y in args.pep_len.split(','):
-            y = y.strip()        
+            y = y.strip()
+            netmhc_name = 'proteome' + y + '_' + x
             p = subprocess.Popen(['python3', 'RunNetMHC.py', project_directory, 'proteome' + y, x, args.rank_filter], cwd=tools_location, stderr=sys.stdout.fileno())
             assert(p.wait() == 0)
-            netmhc_name = 'proteome' + y + '_' + x
-            filtered_netmhc_name = netmhc_name + '_' + args.rank_filter
+            p = subprocess.Popen(['python3', 'FilterNetMHC.py', project_directory, netmhc_name, args.rank_filter, netmhc_name + '_filtered'], cwd=tools_location, stderr=sys.stdout.fileno())
+            assert(p.wait() == 0)
+            filtered_netmhc_name = netmhc_name + '_filtered'
             filtered_netmhc_names.append('--FilteredNetMHC')
             filtered_netmhc_names.append(filtered_netmhc_name)
 
+print('filtered netmhc')
+print(filtered_netmhc_names)
 
-p = subprocess.Popen(['python3', 'CreateTargetSet.py', project_directory] + filtered_netmhc_names, cwd=tools_location, stderr=sys.stdout.fileno())
+p = subprocess.Popen(['python3', 'CreateTargetSet.py', project_directory, 'thing'] + filtered_netmhc_names, cwd=tools_location, stderr=sys.stdout.fileno())
 assert(p.wait() == 0)
 
 p = subprocess.Popen(['python3', 'CreateMSGFPlusIndex.py', project_directory, 'TargetSet', 'thing', 'index'], cwd=tools_location, stderr=sys.stdout.fileno())
@@ -103,6 +107,6 @@ assert(p.wait() == 0)
 p = subprocess.Popen(['python3', 'RunPercolator.py', project_directory, 'msgfplus', 'search', 'percolator'], cwd=tools_location, stderr=sys.stdout.fileno())
 assert(p.wait() == 0)
 
-p = subprocess.Popen(['python3', 'ExportPeptidesWithQValues.py', project_directory, 'percolator', args.output], cwd=tools_location, stderr=sys.stdout.fileno())
+p = subprocess.Popen(['python3', 'ExportPeptidesWithQValues.py', project_directory, 'percolator', os.path.abspath(args.output)], cwd=tools_location, stderr=sys.stdout.fileno())
 assert(p.wait() == 0)
 
