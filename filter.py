e6ad26a147f803b831dfc7f9263ff5461c3d5f46
Jordan Force <jordan.force@uconn.edu>
2019-08-21 10:34:51 -0400

    Created a tool for filtering by Q-value

--- filter.py
+++ filter.py
@@ -12,5 +12,11 @@ args = parser.parse_args()
 
 with open(args.input, 'r') as f:
     with open(args.output, 'w') as g:
-        line = f.readline()
-        g.write(line)
+        header = f.readline()
+        for line in f:
+            parts = line.split()
+            if len(parts) == 2:
+                peptide = parts[0]
+                q_value = float(parts[1])
+                if q_value <= args.threshold:
+                    g.write(peptide + '\n')
