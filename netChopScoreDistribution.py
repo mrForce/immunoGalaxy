d170d7d447431fc43fa3d0267d979e32cad25c5c
Jordan Force <jordan.force@uconn.edu>
2019-10-22 09:12:49 -0400

    Added python script for creating netchop QQ plot, and a test folder with a test in it. Will modify the script to remove PTMs, and then add XML file

--- netChopScoreDistribution.py
+++ netChopScoreDistribution.py
@@ -29,31 +29,34 @@ For each protein, select a random set of positions, and get the scores for them.
 Returns a tuple (positive_scores, control_scores)
 """
 def run_netchop(proteome, peptides, netchop_location, directory):
-    sequences = []
+    sequences = {}
     counter = 1
     for record in SeqIO.parse(proteome, 'fasta'):
         sequence = record.seq
         #consists of the locations to 
         positions_to_score = []
         for x in peptides:
-            start = sequence.seq.find(x, 0)
+            start = sequence.find(x, 0)
             while start > -1:
                 positions_to_score.append(start + len(x))
-                start = sequence.seq.find(x, start + len(x))
+                start = sequence.find(x, start + len(x))
         if positions_to_score:
-            sequence.id = str(counter)
+            record.id = str(counter)
+            record.description = ''
             counter += 1
             positions_to_score = set(positions_to_score)
-            negative_positions = set(range(1, len(sequence.seq) + 1)) - positions_to_score
+            negative_positions = set(range(1, len(sequence) + 1)) - positions_to_score
             control_positions = set(random.sample(list(negative_positions), len(positions_to_score)))
-            sequences[sequence.id] = (sequence, positions_to_score, control_positions)
-
+            sequences[record.id] = (record, positions_to_score, control_positions)
+    print('sequences')
+    print(sequences)
+    assert(sequences)
     output_path = os.path.join(directory, 'netchop_input.fasta')
     output_fp = open(output_path, 'w')
-    SeqIO.write([x[1] for x in sequences], output_fp, 'fasta')
+    SeqIO.write([x[0] for x in sequences.values()], output_fp, 'fasta')
+    output_fp.close()
     netchop_output_path = os.path.join(directory, 'netchop_output.txt')
-    netchop_output_fp = open(netchop_output_path, 'r')
-    subprocess.run([netchop_location + ' ' + output_path + ' > ' + netchop_output_path], shell=True)
+    subprocess.run([netchop_location + ' ' + os.path.abspath(output_path) + ' > ' + os.path.abspath(netchop_output_path)], shell=True)
     positive_scores = []
     control_scores = []
     with open(netchop_output_path, 'r') as f:
@@ -69,93 +72,43 @@ def run_netchop(proteome, peptides, netchop_location, directory):
                 if position in sequences[identity][2]:
                     control_scores.append(score)
     
-        
+    return (positive_scores, control_scores)
             
-    
-def read_scores_as_dict(file_path):
-    d = dict()
-    with open(file_path, 'r') as f:
-        for line in f:
-            line = line.strip()
-            if ',' in line and len(line):
-                split = line.split(',')
-                peptide = split[0]
-                score = float(split[1])
-                assert(peptide not in d)
-                d[peptide] = score
-            else:
-                break
-    return d
 
 
 ptm_removal_regex = re.compile('\[[^\]]*\]')
-def create_qq(scores_dict, peptides, length, output_location):
-    background_scores = []
-    foreground_scores = []
-    for peptide in peptides:
-        if len(peptide) == length:
-            if peptide in scores_dict:
-                foreground_scores.append(math.log(scores_dict[peptide]))
-            else:
-                print('peptide: %s of length %d not in scores_dict' % (peptide, length))
-    for k, v in scores_dict.items():
-        if len(k) == length:
-            background_scores.append(math.log(v))
-    quants = list(np.linspace(0, 1, len(foreground_scores) + 2))[1:-1]
-    assert(len(quants) == len(foreground_scores))
-    background_scores = stats.mstats.mquantiles(background_scores, quants)
-    background_scores.sort()
-    foreground_scores.sort()
-    plt.plot(foreground_scores, background_scores)
-    plt.xlim((foreground_scores[0], foreground_scores[-1]))
-    plt.ylim((background_scores[0], background_scores[-1]))
-    plt.xlabel('Discovered %d-mer Scores' % length)
-    plt.ylabel('Background %d-mer Scores' % length)
-    plt.title('n = %d' % len(foreground_scores))
+def create_qq(positive_scores, control_scores, output_location):
+    assert(all([x <= 1.0 and x >= 0 for x in positive_scores]))
+    assert(all([x <= 1.0 and x >= 0 for x in control_scores]))
+    if len(positive_scores) < len(control_scores):
+        quants = list(np.linspace(0, 1, len(positive_scores) + 2))[1:-1]
+        assert(len(quants) == len(positive_scores))
+        control_scores = stats.mstats.mquantiles(control_scores, quants)
+    control_scores.sort()
+    positive_scores.sort()
+    plt.plot(positive_scores, control_scores)
+    plt.xlim((0, 1))
+    plt.ylim((0, 1))
+    plt.xlabel('Positive Scores')
+    plt.ylabel('Control Scores')
+    plt.title('n = %d' % len(positive_scores))
     plt.savefig(output_location, format='png')
     plt.clf()
+    
 parser = argparse.ArgumentParser()
-parser.add_argument('--allele', type=str)
-parser.add_argument('--base_project', type=str)
+parser.add_argument('--proteome', type=str)
 parser.add_argument('--peptides', type=str)
-parser.add_argument('--output_eight_mers', type=str)
-parser.add_argument('--output_nine_mers', type=str)
-parser.add_argument('--output_ten_mers', type=str)
+parser.add_argument('--qq_output', type=str)
+parser.add_argument('--temp_directory', type=str)
 
 args = parser.parse_args()
 
-eight = tempfile.NamedTemporaryFile()
-eight_name = eight.name
-nine = tempfile.NamedTemporaryFile()
-nine_name = nine.name
-ten = tempfile.NamedTemporaryFile()
-ten_name = ten.name
-base_project = args.base_project
-
-tools_location = '/galaxy-prod/galaxy/tools-dependencies/bin/MSEpitope/tidePipeline'
-p = subprocess.Popen(['python3', 'ExportNetMHCScores.py', base_project, args.allele, '8', eight_name], cwd=tools_location, stderr=sys.stdout.fileno())
-assert(p.wait() == 0)
-p = subprocess.Popen(['python3', 'ExportNetMHCScores.py', base_project, args.allele, '9', nine_name], cwd=tools_location, stderr=sys.stdout.fileno())
-assert(p.wait() == 0)
-p = subprocess.Popen(['python3', 'ExportNetMHCScores.py', base_project, args.allele, '10', ten_name], cwd=tools_location, stderr=sys.stdout.fileno())
-assert(p.wait() == 0)
-peptides = set()
+netchop_location = '/galaxy-prod/galaxy/tools-dependencies/bin/MSEpitope/netchop-3.1/netchop'
+peptides = []
 with open(args.peptides, 'r') as f:
-    for line in f:
-        line = line.strip()
-        if len(line) > 0:
-            cleaned = ptm_removal_regex.sub('', line)
-            if all([x in IUPAC.protein.letters for x in cleaned]):
-                peptides.add(cleaned)
-            else:
-                print('Peptide: %s removed, because it contained non-protein character' % cleaned)
-print('Collected: %d peptides' % len(peptides))
-eight_dict = read_scores_as_dict(eight_name)
-eight.close()
-nine_dict = read_scores_as_dict(nine_name)
-nine.close()
-ten_dict = read_scores_as_dict(ten_name)
-ten.close()
-create_qq(eight_dict, peptides, 8, args.output_eight_mers)
-create_qq(nine_dict, peptides, 9, args.output_nine_mers)
-create_qq(ten_dict, peptides, 10, args.output_ten_mers)
+    for x in f:
+        if len(x.strip()) > 0:
+            peptides.append(x.strip())
+assert(len(peptides) > 0)
+positive_scores, control_scores = run_netchop(args.proteome, peptides, netchop_location, args.temp_directory)
+create_qq(positive_scores, control_scores, args.qq_output)
