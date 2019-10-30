3c6bca3a7a87fc8464f3be920a6ceba84335db81
Jordan Force <jordan.force@uconn.edu>
2019-10-30 09:30:35 -0400

    Adding Ellie tool

---
+++ ellie.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python
+
+import argparse
+import zipfile
+import os
+import fnmatch
+parser = argparse.ArgumentParser()
+parser.add_argument('--archive', type=str)
+parser.add_argument('--positive_fdr', type=float)
+parser.add_argument('--positives_output', type=str)
+parser.add_argument('--unknown_output', type=str)
+
+args = parser.parse_args()
+
+tools_location = '/galaxy-prod/galaxy/tools-dependencies/bin/MSEpitope/tidePipeline'
+
+with zipfile.ZipFile(args.archive, 'r') as zip_object:
+    zip_object.extractall()
+
+project_path = None
+for path, dirs, files in os.walk('.', topdown=True):
+    if len(fnmatch.filter(files, 'database.db')) > 0:
+        project_path = path
+        break
+
+    
+assert(project_path)
+
+p = subprocess.Popen(['python3', 'ExportElliePIN.py', os.path.join(os.getcwd(), project_path), 'search', str(args.positive_fdr), args.positives_output, args.unkown_output], cwd=tools_location, stderr=sys.stdout.fileno())        
+assert(p.wait() == 0)
