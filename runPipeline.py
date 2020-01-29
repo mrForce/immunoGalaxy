0436465497e5d86e3f19867fd13b1da766ba1f86
Jordan Force <jordan.force@uconn.edu>
2020-01-28 19:44:39 -0500

    Changing the way the NetMHC filter works. Do KChop on the additional proteome, then group peptides by length with the base proteome, and take top percent for each length-allele combination. So, it's changing from the top x percent of each additional file to the top x percent of the additional files plus base proteome. Wrote down to-do list

--- runPipeline.py
+++ runPipeline.py
@@ -30,35 +30,84 @@ print(args.pep_len)
 print('output')
 print(args.output)
 
-tools_location = '/galaxy-prod/galaxy/tools-dependencies/bin/MSEpitope/tidePipeline'
+#tools_location = '/galaxy-prod/galaxy/tools-dependencies/bin/MSEpitope/tidePipeline'
+tools_location = '/home/jordan/github/galaxy/tools-dependencies/bin/MSEpitope/tidePipeline'
+
 allele_list = []
 for x in args.allele:
     allele_list.append('--allele')
     allele_list.append(x)
+peptide_lengths = args.pep_len.split(',')
+filtered = False
+if len(arg*s.allele) > 0:
+    assert(len(peptide_lengths) > 0)
+    filtered = True
 project_directory = os.path.join(os.getcwd(), 'project', 'project')
 assert(len(set(args.base_project)) == 1)
 base_project = args.base_project[0]
 print('going to copy project')
 p = subprocess.Popen(['python3', 'CopyProject.py', base_project, project_directory] + allele_list, cwd=tools_location, stderr=sys.stdout.fileno())
 print('copying project')
-filtered_netmhc_names = []
 assert(p.wait() == 0)
-print('going to filter netMHC')
-for allele in args.allele:
-    for pep_len in args.pep_len.split(','):
-        netmhc_name = 'Human' + pep_len + 'Mers_' + allele
-        print('going to call FilterNetMHC. Command: %s' % ' '.join(['python3', 'FilterNetMHC.py', project_directory, netmhc_name, args.rank_filter, netmhc_name + '_filtered']))
-        p = subprocess.Popen(['python3', 'FilterNetMHC.py', project_directory, netmhc_name, args.rank_filter, netmhc_name + '_filtered'], cwd=tools_location, stderr=sys.stdout.fileno())
-        assert(p.wait() == 0)
-        print('called FilterNetMHC.py')
-        filtered_netmhc_names.append('--FilteredNetMHC')
-        filtered_netmhc_names.append(netmhc_name + '_filtered')
 
-print('filtered netMHC')
+length_to_allele_to_netmhc_map = {}
+if filtered:
+    for x in peptide_lengths:
+        for allele in args.allele:
+            length_to_netmhc_map[x][allele] = ['proteome' + x + 'Mers_' + allele]
+            
+
+proteome = 'proteome'
+fasta_link = os.path.join(project_directory, 'proteome.fasta')
+if args.additional_proteome:
+    i = 0
+    additional_fasta_names = []
+    for additional_fasta in args.additional_proteome:
+        name = '%d_add' % i
+        additional_fasta_names.append(name)
+        print('going to call AddFASTA. Command: %s' % ' '.join(command))
+        p = subprocess.Popen(command, cwd=tools_location, stderr=sys.stdout.fileno())
+        assert(p.wait() == 0)
+    if not filtered:
+        """
+        If we're doing NetMHC filtering, then there's no point in combining FASTA files.
+        """
+        command = ['python3', 'ConcatFASTA.py', project_directory, 'cproteome', 'proteome'] + additional_fasta_names
+        print('going to call ConcatFASTA. Command: %s' % ' '.join(command))
+        p = subprocess.Popen(command, cwd=tools_location, stderr=sys.stdout.fileno())
+        assert(p.wait() == 0)
+        proteome = 'cproteome'
+    else:
+        for x in peptide_lengths:
+            x = x.strip()
+            for fasta in additional_fasta_names:
+                peptide_list_name = fasta + '_' + x
+                command = ['python3', 'KChop.py', project_directory, fasta, x, peptide_list_name]
+                print('going to call KChop. Command: %s' % ' '.join(command))
+                p = subprocess.Popen(command, cwd=tools_location, stderr=sys.stdout.fileno())
+                assert(p.wait() == 0)
+                for allele in args.allele:
+                    netmhc_command = ['python3', 'RunNetMHC.py', project_directory, peptide_list_name, allele]
+                    print('going to call RunNetMHC. Command: %s' % ' '.join(netmhc_command))
+                    p = subprocess.Popen(netmhc_command, cwd=tools_location, stderr= sys.stdout.fileno())
+                    assert(p.wait() == 0)
+                    netmhc_name = peptide_list_name + '_' + allele
+                    length_to_allele_to_netmhc_map[x][allele].append(netmhc_name)
+
 print('current working: ' + os.getcwd())
 print('listdir')
 print(os.listdir('.'))
-
+filtered_netmhc_names = []
+if filtered:
+    for x in peptide_lengths:
+        for allele in args.allele:
+            filtered_name = 'joined' + x + 'Mers_' + allele + '_filtered'
+            #notice how I expect FilterNetMHC.py to work. Change this in tidePipeline
+            filter_command = ['python3', 'FilterNetMHC.py', project_directory, args.rank_filter, filtered_name] + length_to_allele_to_netmhc_map[x][allele]
+            print('going to call FilterNetMHC. Command: %s' % ' '.join(filter_command))
+            p = subprocess.Popen(filter_command, cwd=tools_location, stderr=sys.stdout.fileno())
+            assert(p.wait() == 0)
+            filtered_netmhc_names.append(filtered_name)
 add_tools = lambda x: os.path.join(tools_location, x)
 
 
@@ -80,42 +129,6 @@ print('going to call AddMGF. Command: %s' % ' '.join(['python3', 'AddMGF.py', pr
 p = subprocess.Popen(['python3', 'AddMGF.py', project_directory, mgf_link, 'mgf', '8', args.frag_method, args.instrument], cwd=tools_location, stderr=sys.stdout.fileno())
 assert(p.wait() == 0)
 
-
-fasta_link = os.path.join(project_directory, 'proteome.fasta')
-if args.additional_proteome:
-    with open(fasta_link, 'w') as f:
-        for x in args.additional_proteome:
-            with open(x, 'r') as g:
-                shutil.copyfileobj(g, f)
-    print('going to call AddFASTA. Command: %s' % ' '.join(['python3', 'AddFASTA.py', project_directory, fasta_link, 'proteome']))
-    p = subprocess.Popen(['python3', 'AddFASTA.py', project_directory, fasta_link, 'proteome'], cwd=tools_location, stderr=sys.stdout.fileno())
-    assert(p.wait() == 0)
-    for x in args.pep_len.split(','):
-        x = x.strip()
-        print('going to call KChop. Command: %s' % ' '.join(['python3', 'KChop.py', project_directory, 'proteome', x, 'proteome' + x]))
-        p = subprocess.Popen(['python3', 'KChop.py', project_directory, 'proteome', x, 'proteome' + x], cwd=tools_location, stderr=sys.stdout.fileno())
-        
-        assert(p.wait() == 0)
-
-
-    for x in args.allele:
-        for y in args.pep_len.split(','):
-            y = y.strip()
-            netmhc_name = 'proteome' + y + '_' + x
-            netmhc_command = ['python3', 'RunNetMHC.py', project_directory, 'proteome' + y, x]
-            print('going to call RunNetMHC. Command: %s' % ' '.join(netmhc_command))
-            p = subprocess.Popen(netmhc_command, cwd=tools_location, stderr=sys.stdout.fileno())
-            assert(p.wait() == 0)
-            filter_command = ['python3', 'FilterNetMHC.py', project_directory, netmhc_name, args.rank_filter, netmhc_name + '_filtered']
-            print('going to call FilterNetMHC. Command: %s' % ' '.join(filter_command))
-            p = subprocess.Popen(filter_command, cwd=tools_location, stderr=sys.stdout.fileno())
-            assert(p.wait() == 0)
-            filtered_netmhc_name = netmhc_name + '_filtered'
-            filtered_netmhc_names.append('--FilteredNetMHC')
-            filtered_netmhc_names.append(filtered_netmhc_name)
-
-print('filtered netmhc')
-print(filtered_netmhc_names)
 
 if filtered_netmhc_names:
     print('going to call CreateTargetSet. Command: %s' % ' '.join(['python3', 'CreateTargetSet.py', project_directory, 'thing'] + filtered_netmhc_names))
