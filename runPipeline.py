86edf2a37af3d81c18a908488b88c98944afa8da
Jordan Force <jordan.force@uconn.edu>
2020-06-22 11:57:38 -0400

    Modified interface to add option for NetMHC+Percolator. Will test, and then add the feature to the pipeline.

--- runPipeline.py
+++ runPipeline.py
@@ -10,11 +10,13 @@ import itertools
 import uuid
 parser = argparse.ArgumentParser()
 
+TEST=True
 
 parser.add_argument('base_project')
 parser.add_argument('--allele', action='append')
 parser.add_argument('--additional_proteome', action='append')
 parser.add_argument('--mod', action='append')
+parser.add_argument('--mode', choices=['filtered', 'unfiltered', 'netMHCPercolator'])
 parser.add_argument('--pep_len', type=str)
 parser.add_argument('--rank_filter', type=str)
 parser.add_argument('--frag_method', type=str)
@@ -43,11 +45,14 @@ print(args.pep_len)
 
 
 
+
 tools_location = '/galaxy-prod/galaxy/tools-dependencies/bin/MSEpitope/tidePipeline'
 #tools_location = '/home/jordan/github/galaxy/tools-dependencies/bin/MSEpitope/tidePipeline'
 
 allele_list = []
 filtered = False
+if args.mode == 'filtered':
+    filtered = True
 peptide_lengths = []
 if args.allele:
     peptide_lengths = args.pep_len.split(',')
@@ -55,7 +60,6 @@ if args.allele:
         allele_list.append('--allele')
         allele_list.append(x)
     assert(len(peptide_lengths) > 0)
-    filtered = True
 
 
 project_directory = os.path.join(os.getcwd(), 'project', 'project')
@@ -63,9 +67,17 @@ project_directory = os.path.join(os.getcwd(), 'project', 'project')
 
 base_project = args.base_project
 print('going to copy project')
-p = subprocess.Popen(['python3', 'CopyProject.py', base_project, project_directory] + allele_list, cwd=tools_location, stderr=sys.stdout.fileno())
+p = None
+if filtered:
+    if TEST:
+        print('skipping CopyProject because of TEST')
+    else:
+        p = subprocess.Popen(['python3', 'CopyProject.py', base_project, project_directory] + allele_list, cwd=tools_location, stderr=sys.stdout.fileno())
+else:
+    p = subprocess.Popen(['python3', 'CopyProject.py', base_project, project_directory], cwd=tools_location, stderr=sys.stdout.fileno())
 print('copying project')
-assert(p.wait() == 0)
+if not TEST:
+    assert(p.wait() == 0)
 
 with tempfile.NamedTemporaryFile(mode='w', delete=False) as temp_mod:
     if args.mod:
@@ -74,8 +86,11 @@ with tempfile.NamedTemporaryFile(mode='w', delete=False) as temp_mod:
     temp_mod.flush()
     command = ['python3', 'AddModificationFile.py', project_directory, 'mod', temp_mod.name]
     print('Going to add modification file: ' + ' '.join(command))
-    p = subprocess.Popen(command, cwd=tools_location, stderr=sys.stdout.fileno())
-    assert(p.wait() == 0)
+    if TEST:
+        print('Skipping AddModificationFile because of TEST')
+    else:
+        p = subprocess.Popen(command, cwd=tools_location, stderr=sys.stdout.fileno())
+        assert(p.wait() == 0)
 
 length_to_allele_to_netmhc_map = {}
 if filtered:
@@ -95,16 +110,22 @@ if args.additional_proteome:
         additional_fasta_names.append(name)
         command = ['python3', 'AddFASTA.py', project_directory, additional_fasta, name]
         print('going to call AddFASTA. Command: %s' % ' '.join(command))
-        p = subprocess.Popen(command, cwd=tools_location, stderr=sys.stdout.fileno())
-        assert(p.wait() == 0)
+        if TEST:
+            print('skipping AddFASTA because of TEST')
+        else:
+            p = subprocess.Popen(command, cwd=tools_location, stderr=sys.stdout.fileno())
+            assert(p.wait() == 0)
     if not filtered:
         """
         If we're doing NetMHC filtering, then there's no point in combining FASTA files.
         """
         command = ['python3', 'ConcatFASTA.py', project_directory, '--source', 'cproteome', 'proteome'] + additional_fasta_names
         print('going to call ConcatFASTA. Command: %s' % ' '.join(command))
-        p = subprocess.Popen(command, cwd=tools_location, stderr=sys.stdout.fileno())
-        assert(p.wait() == 0)
+        if TEST:
+            print('skipping ConcatFASTA because of TEST')
+        else:
+            p = subprocess.Popen(command, cwd=tools_location, stderr=sys.stdout.fileno())
+            assert(p.wait() == 0)
         proteome = 'cproteome'
     else:
         for x in peptide_lengths:
@@ -113,13 +134,19 @@ if args.additional_proteome:
                 peptide_list_name = fasta + '_' + x
                 command = ['python3', 'KChop.py', project_directory, fasta, x, peptide_list_name]
                 print('going to call KChop. Command: %s' % ' '.join(command))
-                p = subprocess.Popen(command, cwd=tools_location, stderr=sys.stdout.fileno())
-                assert(p.wait() == 0)
+                if TEST:
+                    print('skipping KChop because of TEST')
+                else:
+                    p = subprocess.Popen(command, cwd=tools_location, stderr=sys.stdout.fileno())
+                    assert(p.wait() == 0)
                 for allele in args.allele:
                     netmhc_command = ['python3', 'RunNetMHC.py', project_directory, peptide_list_name, allele]
                     print('going to call RunNetMHC. Command: %s' % ' '.join(netmhc_command))
-                    p = subprocess.Popen(netmhc_command, cwd=tools_location, stderr= sys.stdout.fileno())
-                    assert(p.wait() == 0)
+                    if TEST:
+                        print('skipping RunNetMHC because of TEST')
+                    else:
+                        p = subprocess.Popen(netmhc_command, cwd=tools_location, stderr= sys.stdout.fileno())
+                        assert(p.wait() == 0)
                     netmhc_name = peptide_list_name + '_' + allele
                     length_to_allele_to_netmhc_map[x][allele].append(netmhc_name)
 
@@ -134,8 +161,11 @@ if filtered:
             #notice how I expect FilterNetMHC.py to work. Change this in tidePipeline
             filter_command = ['python3', 'FilterNetMHC.py', project_directory, args.rank_filter, filtered_name] + length_to_allele_to_netmhc_map[x][allele]
             print('going to call FilterNetMHC. Command: %s' % ' '.join(filter_command))
-            p = subprocess.Popen(filter_command, cwd=tools_location, stderr=sys.stdout.fileno())
-            assert(p.wait() == 0)
+            if TEST:
+                print('skipping FilterNetMHC because of TEST')
+            else:
+                p = subprocess.Popen(filter_command, cwd=tools_location, stderr=sys.stdout.fileno())
+                assert(p.wait() == 0)
             filtered_netmhc_names.append(filtered_name)
 add_tools = lambda x: os.path.join(tools_location, x)
 
@@ -155,26 +185,39 @@ mgf_link = os.path.join(project_directory, 'thing.mgf')
 os.symlink(args.mgf, mgf_link)
 
 print('going to call AddMGF. Command: %s' % ' '.join(['python3', 'AddMGF.py', project_directory, mgf_link, 'mgf', '8', args.frag_method, args.instrument]))
-p = subprocess.Popen(['python3', 'AddMGF.py', project_directory, mgf_link, 'mgf', '8', args.frag_method, args.instrument], cwd=tools_location, stderr=sys.stdout.fileno())
-assert(p.wait() == 0)
+if TEST:
+    print('skipping AddMGF because of TEST')
+else:
+    p = subprocess.Popen(['python3', 'AddMGF.py', project_directory, mgf_link, 'mgf', '8', args.frag_method, args.instrument], cwd=tools_location, stderr=sys.stdout.fileno())
+    assert(p.wait() == 0)
+
 
 
 if filtered_netmhc_names:
     command = ['python3', 'CreateTargetSet.py', project_directory, 'targetSet'] + list(itertools.chain.from_iterable([('--FilteredNetMHC', x) for x in filtered_netmhc_names]))
     print('going to call CreateTargetSet. Command: %s' % ' '.join(command))
-    p = subprocess.Popen(command, cwd=tools_location, stderr=sys.stdout.fileno())
-    assert(p.wait() == 0)
+    if TEST:
+        print('skipping CreateTargetSet because of TEST')
+    else:
+        p = subprocess.Popen(command, cwd=tools_location, stderr=sys.stdout.fileno())
+        assert(p.wait() == 0)
     print('created target set')
 
     command = ['python3', 'CreateMSGFPlusIndex.py', project_directory, 'TargetSet', 'targetSet', 'index']
     print('going to call CreateMSGFPlusIndex. Command: %s' % ' '.join(command))
-    p = subprocess.Popen(command, cwd=tools_location, stderr=sys.stdout.fileno())
-    assert(p.wait() == 0)
+    if TEST:
+        print('skipping CreateMSGFPlusIndex because of TEST')
+    else:
+        p = subprocess.Popen(command, cwd=tools_location, stderr=sys.stdout.fileno())
+        assert(p.wait() == 0)
 else:
     command = ['python3', 'CreateMSGFPlusIndex.py', project_directory, 'FASTA', proteome, 'index']
     print('going to call CreateMSGFPlusIndex. Command: %s' % ' '.join(command))
-    p = subprocess.Popen(command, cwd=tools_location, stderr=sys.stdout.fileno())
-    assert(p.wait() == 0)
+    if TEST:
+        print('skipping CreateMSGFPlusIndex because of TEST')
+    else:
+        p = subprocess.Popen(command, cwd=tools_location, stderr=sys.stdout.fileno())
+        assert(p.wait() == 0)
     
 print('created msgfplus index')
 command = ['python3', 'RunMSGFPlusSearch.py', project_directory, 'mgf', 'index', 'search', '--modifications_name', 'mod', '--memory', '10000', '--thread', '4', '--n', str(args.num_matches_per_spectrum), '--t', args.precursor_tolerance]
@@ -185,14 +228,24 @@ if args.maxLength > 0:
     command.append('--maxLength')
     command.append(str(args.maxLength))
 print('going to call RunMSGFPlusSearch. Command: %s' % ' '.join(command))
-p = subprocess.Popen(command, cwd=tools_location, stderr=sys.stdout.fileno())
-assert(p.wait() == 0)
+if TEST:
+    print('skipping RunMSGFPlusSearch because of TEST')
+else:
+    p = subprocess.Popen(command, cwd=tools_location, stderr=sys.stdout.fileno())
+    assert(p.wait() == 0)
 print('ran msgfplus search')
 
 command = ['python3', 'RunPercolator.py', project_directory, 'msgfplus', 'search', 'percolator', '--num_matches_per_spectrum', str(args.num_matches_per_spectrum)]
+if args.mode == 'netMHCPercolator':
+    for x in args.alleles:
+        command.append('--allele')
+        command.append(x)
 print('going to call RunPercolator. Command: %s' % ' '.join(command))
-p = subprocess.Popen(command, cwd=tools_location, stderr=sys.stdout.fileno())
-assert(p.wait() == 0)
+if TEST:
+    print('skipping RunPercolator because of TEST')
+else:
+    p = subprocess.Popen(command, cwd=tools_location, stderr=sys.stdout.fileno())
+    assert(p.wait() == 0)
 
 print('ran percolator')
 #print('going to call ExportPeptidesWithQValues. Command: %s' % ' '.join(['python3', 'ExportPeptidesWithQValues.py', project_directory, 'percolator', os.path.abspath(args.peptide_output)]))
@@ -207,8 +260,11 @@ print('ran percolator')
 
 command = ['python3', 'ExportElliePIN.py', project_directory, 'search', '0.01', '/dev/null', args.msgf_unfiltered]
 print('going to call ExportElliePIN. Command: %s' % ' '.join(command))
-p = subprocess.Popen(command, cwd=tools_location, stderr=sys.stdout.fileno())
-assert(p.wait() == 0)
+if TEST:
+    print('skipping ExportElliePIN because of TEST')
+else:
+    p = subprocess.Popen(command, cwd=tools_location, stderr=sys.stdout.fileno())
+    assert(p.wait() == 0)
 print('got ellie outputs')
 
 
