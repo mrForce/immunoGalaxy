2e96fc0dbd12ca4c284227b6c6696e1c3ec5f939
Jordan Force <jordan.force@uconn.edu>
2021-01-04 16:36:38 -0500

    Need to finish filterNetMHC

--- precomputeNetMHC/filterNetMHC.py
+++ precomputeNetMHC/filterNetMHC.py
@@ -8,14 +8,13 @@ from precomputedNetMHCIndex import ChainCollection, peptideGenerator, ScoreTable
 def getPeptideGen(chainCollection, fastaPath, pepLen):
     return map(lambda x: x.getPeptideSequence(), peptideGenerator(chainCollection, fastaPath, pepLen))
 
-def scoreDistribution(table, allele, peptideGen, peptidesToExclude):
-    #peptidesToExclude must be a set
-    allelePosition = table.getAlleleList().index(allele)
+def scoreDistribution(scoreIter, peptideGen, peptidesToExclude):
     count = collections.Counter()
-    for peptide, row in itertools.zip_longest(peptideGen, table):
+    for peptide, score in itertools.zip_longest(peptideGen, scoreIter):
         if peptide not in peptidesToExclude:
-            count[row[allelePosition]] += 1
+            count[score] += 1
     return count
+
 
 def computeScoreThreshold(distribution, k):
     numPeptides = sum(distribution.values())
@@ -40,12 +39,11 @@ def extractPeptidesAndScores(scoreIter, peptideHolderGen, indices):
         i += 1
             
 
-def filterOnThreshold(table, allele, peptideHolderGen, threshold):
-    allelePosition = table.getAlleleList().index(allele)
+def filterOnThreshold(scoreIter, peptideHolderGen, threshold):
     peptides = set()
-    for peptideHolder, row in itertools.zip_longest(peptideHolderGen, scoreTable):
-        if row[allelePosition] <= threshold:
-            peptides.add(peptideHolder)
+    for peptideHolder, score in itertools.zip_longest(peptideHolderGen, scoreIter):
+        if score <= threshold:
+            peptides.add(peptideHolder.getPeptideSequence())
     return peptides
     
 
@@ -67,7 +65,7 @@ def filterNetMHC(allele, length, baseScoreTable, baseChainCollection, baseFasta,
     threshold = computeScoreThreshold(combinedScoreDist, k)
     peptides = set()
     if baseScoreTable and baseChainCollection and baseFasta:
-        peptides += filterOnThreshold(baseScoreTable, allele, getPeptideGen(baseChainCollection, baseFasta, length), threshold)
+        peptides.update(filterOnThreshold(baseScoreTable, allele, getPeptideGen(baseChainCollection, baseFasta, length), threshold))
     if additionalScoreTable and additionalChainCollection and additionalFasta:
-        peptides += filterOnThreshold(additionalScoreTable, allele, getPeptideGen(additionalChainCollection, additionalFasta, length), threshold)
+        peptides.update(filterOnThreshold(additionalScoreTable, allele, getPeptideGen(additionalChainCollection, additionalFasta, length), threshold))
     return peptides
