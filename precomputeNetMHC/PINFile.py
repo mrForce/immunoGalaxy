383250324e16175586e6874c02bae2f0833d586f
Jordan Force <jordan.force@uconn.edu>
2021-01-07 15:02:22 -0500

    Created runPipeline script. Need to add location of tools and test

---
+++ precomputeNetMHC/PINFile.py
@@ -0,0 +1,45 @@
+import csv
+import re
+def parse_peptide(peptide, peptide_regex, ptm_removal_regex):
+    match = peptide_regex.match(peptide)
+    if match and match.group('peptide'):
+        matched_peptide = match.group('peptide')
+        return ptm_removal_regex.sub('', matched_peptide)
+    else:
+        return None
+
+class PINFile:
+    def __init__(self, path):
+        self.path = path
+        self.peptides = set()
+        self.peptide_regex = re.compile('^[A-Z\-]\.(?P<peptide>.*)\.[A-Z\-]$')
+        self.ptm_removal_regex = re.compile('\[[^\]]*\]')
+        with open(path, 'r') as f:
+            reader = csv.DictReader(f, delimiter='\t', restkey='Proteins')
+            self.fieldnames = list(reader.fieldnames)
+            next(reader)
+            for row in reader:
+                peptide = parse_peptide(row['Peptide'], self.peptide_regex, self.ptm_removal_regex)
+                assert(peptide)
+                self.peptides.add(peptide)
+    @property
+    def peptides(self):
+        return self.peptides
+    def addScores(self, scoreDict, columnHeader, columnDirection):
+        #inserts the scores
+        rows = []
+        with open(self.path, 'r') as f:
+            reader = csv.DictReader(f, self.fieldnames, delimiter='\t', restkey='Proteins')
+            direction = dict(next(reader))
+            for row in reader:
+                rows.append(row)
+        self.fieldnames.insert(6, columnHeader)
+        direction[columnHeader] = columnDirection
+        with open(self.path, 'w') as f:
+            writer = csv.DictWriter(f, self.fieldnames, delimiter='\t')
+            writer.writeheader()
+            writer.writerow(direction)
+            for row in rows:
+                row_copy = dict(row)
+                row_copy[columnHeader] = str(scoreDict[row['Peptide']])
+                writer.writerow(row_copy)
